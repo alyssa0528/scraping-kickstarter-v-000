4e42a33df402627dd0bf12e42137ae83f7c0946f
Alyssa Kim <alyssa.kim@gmail.com>
2018-03-06 15:46:21 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,8 +4,8 @@ require 'nokogiri'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
-binding.pry
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
 
 create_project_hash
